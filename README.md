c4dbe8238417890b6ac9d267eba9e0f8bc89e313
yhattm <yhattm@gmail.com>
2020-03-24 16:42:32 +0800

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# DeviceQRCodeGenerator
-Created with CodeSandbox
---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import "./styles.css";
+import QRCode from "qrcode.react";
+import { TextField, MenuItem } from "@material-ui/core";
+
+const App = () => {
+  const [token, setToken] = useState("");
+  const [ssid, setSSID] = useState("bestvvtk");
+  const [ip, setIP] = useState("192.168.0.1");
+  const [type, setType] = useState("radar");
+
+  const msgObj = { token, ssid, ip, type };
+  const msgStr = JSON.stringify(msgObj);
+  return (
+    <div className="App">
+      <h1>Device QR Code Generator</h1>
+      <div />
+      <TextField
+        label="Token"
+        value={token}
+        onChange={event => {
+          setToken(event.target.value);
+        }}
+      />
+      <div />
+      <TextField
+        label="SSID"
+        value={ssid}
+        onChange={event => {
+          setSSID(event.target.value);
+        }}
+      />
+      <div />
+      <TextField
+        label="IP"
+        value={ip}
+        onChange={event => {
+          setIP(event.target.value);
+        }}
+      />
+      <div />
+      <TextField
+        label="Type"
+        value={type}
+        select
+        onChange={event => {
+          setType(event.target.value);
+        }}
+      >
+        <MenuItem value="radar">Radar</MenuItem>
+        <MenuItem value="camera">Camera</MenuItem>
+        <MenuItem value="doorbell">Doorbell</MenuItem>
+      </TextField>
+      <p>QRCode msg is {msgStr}</p>
+      <div />
+      <QRCode value={msgStr} includeMargin={true} />
+    </div>
+  );
+};
+
+export default App;
